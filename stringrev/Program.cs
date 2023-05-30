d8ba1ecb0eac5a3fac3cb434cf9e53fd503e6c2d
navenr <132997178+navenr@users.noreply.github.com>
2023-05-30 14:16:54 +0530

    Add files via upload

---
+++ stringrev/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringrev
+{
+    class sample
+    {
+        static void Main()
+        {
+            // original string
+            string str = "GOPAL Perumal";
+            // reverse the string
+            string rev = string.Join(" ", str.Split(' ').Select(s => new String(s.Reverse().ToArray())));
+            Console.WriteLine(rev);
+        } 
+    }
+}
