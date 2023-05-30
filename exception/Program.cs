c0016ef1bcbf46ab6deff6ee9e94d746b84f7319
navenr <132997178+navenr@users.noreply.github.com>
2023-05-30 15:12:31 +0530

    Add files via upload

---
+++ exception/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5] { 10, 30, 25, 45, 65 };
+
+
+            if (arr.Length >= 6)
+                Console.WriteLine(arr[5]);
+
+            try
+            {
+                Console.WriteLine(arr[0]);
+                Console.WriteLine(arr[1]);
+                Console.WriteLine(arr[2]);
+                Console.WriteLine(arr[3]);
+                Console.WriteLine(arr[4]);
+                Console.WriteLine(arr[5]);
+            
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error:{0}",ex.Message);
+
+            }
+            finally 
+            { Console.WriteLine();
+            }
+        }
+    }
+}
+
